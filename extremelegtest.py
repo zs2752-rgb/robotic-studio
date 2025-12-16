bbe3adb263e44d316823665a5392810c1ad0abfa
zs2752-rgb <zs2752@columbia.edu>
2025-12-16 17:50:20 -0500

    Update extremelegtest.py

--- extremelegtest.py
+++ extremelegtest.py
@@ -43,18 +43,18 @@ t = 0
 
 
 time.sleep(3)
-servo1.move(210)
-servo2.move(10)
+servo1.move(120)
+servo2.move(120)
 
 time.sleep(3)
-servo3.move(20)
-servo4.move(230)
+servo3.move(120)
+servo4.move(120)
 
 time.sleep(3)
-servo5.move(210)
-servo6.move(230)
+servo5.move(120)
+servo6.move(120)
 
 time.sleep(3)
-servo7.move(20)
-servo8.move(10)
+servo7.move(120)
+servo8.move(120)
 time.sleep(3)
